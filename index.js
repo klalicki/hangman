759f447191a47add57be315cfcaecfdf453598c8
klalicki <32229646+klalicki@users.noreply.github.com>
2022-11-21 10:45:42 -0500

    fully refactored code to use a Class

--- index.js
+++ index.js
@@ -10,125 +10,112 @@ const selectWord = () => {
   return wordBank[wordIndex];
 };
 
-/**
- * prompts the user to input a letter, and if the input is not a letter, prompts the user to
- * input a letter again. Returns the lowercase of that letter
- * @returns A function that takes no arguments and returns a string.
- */
-const inputLetter = () => {
-  let isValidInput = false;
-  let input;
-  while (!isValidInput) {
-    input = prompt.question("Please enter a letter: ", {}).toLowerCase();
-    //validate input
-    // test to make sure the input only contains letters
-    if (!/[A-Za-z]/.test(input)) {
-      console.log("Invalid input: please input a letter");
-      //test to make sure only one letter
-    } else if (input.length != 1) {
-      console.log("Invalid input: please input a letter");
-      //test to see if the letter has been guessed already
-    } else if (gameData.guessedLetters.includes(input)) {
-      console.log(`You have already guessed ${input}. Try a different letter.`);
-    } else {
-      return input;
-    }
+class WordGame {
+  constructor(word, numberOfLives = 6) {
+    this.livesRemaining = numberOfLives;
+    this.word = word;
+    this.wordLength = word.length;
+    this.letters = word.split("");
+    this.guessedLetters = [];
+    this.displayLetters = this.letters.map(() => {
+      return "_";
+    });
   }
-  return input.toLowerCase();
-};
-
-/**
- * Checks to see if a letter is part of the word, returns a boolean. Add the letter to the list of guesses, sort that list.
- * @param letter - The letter that the user typed.
- * @returns A boolean value.
- */
-const checkLetter = (letter) => {
-  //add letter to the guesses list
-  gameData.guessedLetters.push(letter);
-  gameData.guessedLetters.sort();
-  return gameData.letters.includes(letter);
-};
-
-/**
- * Loops through the word and reveals the letter that was passed to it.
- * This function edits the gameData object, and does not return anything.
- * @param letter - the letter that was guessed
- */
-const revealLetter = (letter) => {
-  gameData.letters.forEach((wordLetter, index) => {
-    if (wordLetter === letter) {
-      gameData.displayLetters[index] = wordLetter;
+  /**
+   * this function renders the game interface, such as the number of lives remaining, list of letters guessed, etc
+   */
+  render() {
+    console.clear();
+    console.log(
+      `remaining lives: ${"❤ ".repeat(this.livesRemaining)}${"- ".repeat(
+        6 - this.livesRemaining
+      )}`
+    );
+    console.log(`word so far: ${this.displayLetters.join(" ")}`);
+    console.log(`letters guessed so far: ${this.guessedLetters}`);
+  }
+  /**
+   * prompts the user to input a letter, and if the input is not a letter, prompts the user to
+   * input a letter again. Returns the lowercase of that letter
+   * @returns A function that takes no arguments and returns a string.
+   */
+  inputLetter = () => {
+    let isValidInput = false;
+    let input;
+    while (!isValidInput) {
+      input = prompt.question("Please enter a letter: ", {}).toLowerCase();
+      //validate input
+      // test to make sure the input only contains letters
+      if (!/[A-Za-z]/.test(input)) {
+        console.log("Invalid input: please input a letter");
+        //test to make sure only one letter
+      } else if (input.length != 1) {
+        console.log("Invalid input: please input a letter");
+        //test to see if the letter has been guessed already
+      } else if (this.guessedLetters.includes(input)) {
+        console.log(
+          `You have already guessed ${input}. Try a different letter.`
+        );
+      } else {
+        return input;
+      }
     }
-  });
-};
-
-/**
- * Generates an object containing all the data for the current round.
- * @returns An object
- */
-const generateGameData = () => {
-  const livesRemaining = 6;
-  const word = selectWord();
-  const wordLength = word.length;
-  const letters = word.split("");
-  const guessedLetters = [];
-  const displayLetters = letters.map(() => {
-    return "_";
-  });
-  return {
-    word,
-    wordLength,
-    letters,
-    displayLetters,
-    livesRemaining,
-    guessedLetters,
+    return input.toLowerCase();
   };
-};
+  /**
+   * Loops through the word and reveals the letter that was passed to it.
+   * This function edits the gameData object, and does not return anything.
+   * @param letter - the letter that was guessed
+   */
+  revealLetter = (letter) => {
+    this.letters.forEach((wordLetter, index) => {
+      if (wordLetter === letter) {
+        this.displayLetters[index] = wordLetter;
+      }
+    });
+  };
+  /**
+   * Checks to see if a letter is part of the word, returns a boolean. Add the letter to the list of guesses, sort that list.
+   * @param letter - The letter that the user typed.
+   * @returns A boolean value.
+   */
+  checkLetter = (letter) => {
+    //add letter to the guesses list
+    this.guessedLetters.push(letter);
+    this.guessedLetters.sort();
+    return this.letters.includes(letter);
+  };
 
-/**
- * this function renders the game interface, such as the number of lives remaining, list of letters guessed, etc
- */
-const renderGameData = () => {
-  console.clear();
-  console.log(
-    `remaining lives: ${"❤ ".repeat(gameData.livesRemaining)}${"- ".repeat(
-      6 - gameData.livesRemaining
-    )}`
-  );
-  console.log(`word so far: ${gameData.displayLetters.join(" ")}`);
-  console.log(`letters guessed so far: ${gameData.guessedLetters}`);
-};
+  /**
+   * Plays one round of the game.
+   *  */
+  play = () => {
+    // main game loop - repeats as long as there are guesses remaining and letters to be guessed.
+    while (this.livesRemaining > 0 && this.displayLetters.includes("_")) {
+      this.render();
+      const roundLetter = this.inputLetter();
 
-let gameData;
-
-/**
- * executes a round of the game.
- */
-const runGame = () => {
-  //generate the gameData object
-  gameData = generateGameData();
-  console.log(gameData.word);
-
-  // main game loop - repeats as long as there are guesses remaining and letters to be guessed.
-  while (gameData.livesRemaining > 0 && gameData.displayLetters.includes("_")) {
-    renderGameData();
-    const roundLetter = inputLetter();
-
-    if (checkLetter(roundLetter)) {
-      console.log("correct!");
-      revealLetter(roundLetter);
-    } else {
-      console.log("incorrect!");
-      gameData.livesRemaining--;
+      if (this.checkLetter(roundLetter)) {
+        console.log("correct!");
+        this.revealLetter(roundLetter);
+      } else {
+        console.log("incorrect!");
+        this.livesRemaining--;
+      }
     }
-  }
-  //game is over! check if the player has won or lost
-  if (gameData.displayLetters.includes("_")) {
-    //player has lost.
-    console.log(`you lost, sorry. the word was ${gameData.word}`);
-  } else {
-    console.log("congrats! you win! :)");
-  }
-};
+    //game is over! check if the player has won or lost
+    if (this.displayLetters.includes("_")) {
+      //player has lost.
+      console.log(`you lost, sorry. the word was ${this.word}`);
+    } else {
+      console.log("congrats! you win! :)");
+    }
+  };
+}
 
-runGame();
+//
+console.log("Welcome to %cHangman", "color:red");
+
+const word = selectWord();
+const game = new WordGame(word);
+// game.play();
